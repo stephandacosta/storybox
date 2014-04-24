3522bf0bcfcd6fa6675d29daacd123fa3b93881a
stephandacosta <stephan.dacosta.tech@gmail.com>
2014-04-24 12:21:59 -0700

    fisheye testing initial commit

--- fisheye.js
+++ fisheye.js
@@ -1,120 +1,131 @@
-// Fisheye Distortion
-// Demo: http://bost.ocks.org/mike/fisheye/
+var Fisheye = {
+   $container: $("body"),
 
-// Implements a fisheye distortion for two-dimensional layouts. Based on Sarkar and Brown’s Graphical Fisheye Views of Graphs (CHI '92), as well as Flare's FisheyeDistortion and Sigma.js's fisheye example.
+   init: function($container){
+       console.log('creating fisheye');
+       Fisheye.$container = $container;
+       sentimentHub.on('Data', Fisheye.onData);
+       sentimentHub.on('Done', Fisheye.onDataDone);
+       sentimentHub.fetch();
+   },
 
-// When constructing a fisheye distortion, you can specify the radius and distortion factor:
+   // Triggered when data for a particular network is available
+   onData: function(network, data){
 
-// var fisheye = d3.fisheye.circular()
-//     .radius(200)
-//     .distortion(2);
-// Typically, you then update the focal point of the distortion on mousemove:
+   },
 
-// svg.on("mousemove", function() {
-//   fisheye.focus(d3.mouse(this));
-// });
-// The distortion operator takes as input an object with x and y attributes, and returns a new object with x, y and z attributes. The returned object represents the distorted position of the input object; the z property is a scaling factor so that you can optionally distort the size of elements as well.
+   // Triggered when we are done fetching data
+   onDataDone: function(data){
+       console.log('fisheye on data done');
+       console.log(data);
+       Fsheye.drawMosaic(data.slice(0,20));
+   },
 
-// For example, to apply fisheye distortion to a force layout, stash the distorted positions in a display property on each node, and then use the distorted positions to update the nodes and links:
+   playIfShared: function(item){
+       var contentId = $.cookie("vgvidid");
+       if (contentId && item.id == contentId){
+           ga_events.sboxShareReferral(item.network, item.id);
+           if (item.type != 'video'){
+               player.hidePlayerMessage();
+               player.play(item);
+               ga_events.shareReferral('content:'+item.network, item.id);
+               $.cookie("vgvidid", '', {
+                   expires: 0,
+                   domain: cookieDomain,
+                   path: '/'
+               });
+           }
+       }
+   },
 
-// svg.on("mousemove", function() {
-//   fisheye.focus(d3.mouse(this));
+   // draw the items as desired
+   drawItem: function(network, item){
 
-//   node.each(function(d) { d.fisheye = fisheye(d); })
-//       .attr("cx", function(d) { return d.fisheye.x; })
-//       .attr("cy", function(d) { return d.fisheye.y; })
-//       .attr("r", function(d) { return d.fisheye.z * 4.5; });
+       // For this example I am creating a separate network div for each network data and
+       // append the items to the network div...
+       console.log('network', network);
+       var $networkDiv = $("."+network, Example.$container);
+       if ($networkDiv.length < 1) 
+           $networkDiv = $("<div></div>").addClass("network").addClass(network)
+               .append($("<h2></h2>").html(network))
+               .appendTo(Example.$container);
 
-//   link.attr("x1", function(d) { return d.source.fisheye.x; })
-//       .attr("y1", function(d) { return d.source.fisheye.y; })
-//       .attr("x2", function(d) { return d.target.fisheye.x; })
-//       .attr("y2", function(d) { return d.target.fisheye.y; });
-// });
-// There's also a d3.fisheye.scale for Cartesian distortion; see the above demo for an example.
+       console.log("Item:", item);
+
+       // create a div and attach the raw item data so we can use it to open the player popup on click
+       var photo = item.image || item.thumb || item.profilePic;
+       $networkDiv.append(
+           $("<div></div>").addClass("item") 
+               .append(photo? $("<img />").attr({src: photo}) : '')
+               .append($("<div></div>").addClass("info").html(item.text))
+               .click(function(){
+                   player.play(item);
+               })
+               .data({item: item})
+       );
+   },
+
+   drawMosaic : function (data){
+
+       var width = 960,
+           height = 500,
+          thumbr = 80,
+          // centerRadius = Math.min(width/2-thumbr, height/2-thumbr);
+
+       var svg = d3.select("body").append("svg")
+           .attr("width", width)
+           .attr("height", height);
 
 
-(function() {
-  d3.fisheye = {
-    scale: function(scaleType) {
-      return d3_fisheye_scale(scaleType(), 3, 0);
-    },
-    circular: function() {
-      var radius = 200,
-          distortion = 2,
-          k0,
-          k1,
-          focus = [0, 0];
+        for (var i = 0 ; i < data.length ; i++){
+            var rectangle = svgContainer.append("rect")
+                .attr("x", i*width/data.length)
+                .attr("y", 0)
+                .attr("width", width/data.length)
+                .attr("height", height/2);
 
-      function fisheye(d) {
-        var dx = d.x - focus[0],
-            dy = d.y - focus[1],
-            dd = Math.sqrt(dx * dx + dy * dy);
-        if (!dd || dd >= radius) return {x: d.x, y: d.y, z: 1};
-        var k = k0 * (1 - Math.exp(-dd * k1)) / dd * .75 + .25;
-        return {x: focus[0] + dx * k, y: focus[1] + dy * k, z: Math.min(k, 10)};
-      }
+            rectangle.append("clipPath")
+               .attr("id", function(d) { return "rect" + d.index; })
+               .append("rectangle")
+                .attr("x", i*width/data.length);
+                .attr("y", 0)
+                .attr("width", width/data.length)
+                .attr("height", height/2);
 
-      function rescale() {
-        k0 = Math.exp(distortion);
-        k0 = k0 / (k0 - 1) * radius;
-        k1 = distortion / radius;
-        return fisheye;
-      }
+            rectangle.append("image")
+                .attr("xlink:href", function(d) { return d.thumb; })
+                .attr("clip-path", function(d) { return "url(#rect" + d.index + ")";})
+                .attr("x", -50)
+                .attr("y", -50)
+                .attr("width", 100)
+                .attr("height", 100);
+        }
 
-      fisheye.radius = function(_) {
-        if (!arguments.length) return radius;
-        radius = +_;
-        return rescale();
-      };
 
-      fisheye.distortion = function(_) {
-        if (!arguments.length) return distortion;
-        distortion = +_;
-        return rescale();
-      };
 
-      fisheye.focus = function(_) {
-        if (!arguments.length) return focus;
-        focus = _;
-        return fisheye;
-      };
+         // node.on("mouseover", function() {
+         //   d3.select(this).transition()
+         //     .style("fill", "white")
+         //     .attr("r", 40)
+         //     .attr("cx", 0)
+         //     .attr("cy", 0)
+         //   .duration(1000)
+         //   .delay(100);
 
-      return rescale();
-    }
-  };
+           // d3.select(this).append("text")
+           //   .attr("dx", -20)
+           //   .attr("dy", 30)
+           //   .text(function(d) { return d.contribName; } );
+         //});
 
-  function d3_fisheye_scale(scale, d, a) {
+         // node.on("click", function(d){
+         //           player.play(d);
+         //       });
 
-    function fisheye(_) {
-      var x = scale(_),
-          left = x < a,
-          range = d3.extent(scale.range()),
-          min = range[0],
-          max = range[1],
-          m = left ? a - min : max - a;
-      if (m == 0) m = max - min;
-      return (left ? -1 : 1) * m * (d + 1) / (d + (m / Math.abs(x - a))) + a;
-    }
 
-    fisheye.distortion = function(_) {
-      if (!arguments.length) return d;
-      d = +_;
-      return fisheye;
-    };
+   }
 
-    fisheye.focus = function(_) {
-      if (!arguments.length) return a;
-      a = +_;
-      return fisheye;
-    };
 
-    fisheye.copy = function() {
-      return d3_fisheye_scale(scale.copy(), d, a);
-    };
 
-    fisheye.nice = scale.nice;
-    fisheye.ticks = scale.ticks;
-    fisheye.tickFormat = scale.tickFormat;
-    return d3.rebind(fisheye, scale, "domain", "range");
-  }
-})();
+
+};
