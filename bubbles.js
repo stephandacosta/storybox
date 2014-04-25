86a10db9040dfd0b532a62416e0e821d104e8100
stephandacosta <stephan.dacosta.tech@gmail.com>
2014-04-24 21:15:12 -0700

    added hover method, added player in middle

--- bubbles.js
+++ bubbles.js
@@ -1,130 +1,144 @@
 var Bubble = {
-   $container: $("body"),
+  $container: $("body"),
 
-   init: function($container){
-       console.log('creating bubble');
-       Bubble.$container = $container;
-       sentimentHub.on('Data', Bubble.onData);
-       sentimentHub.on('Done', Bubble.onDataDone);
-       sentimentHub.fetch();
-   },
+  init: function($container){
+    console.log('creating bubble');
+    Bubble.$container = $container;
+    sentimentHub.on('Data', Bubble.onData);
+    sentimentHub.on('Done', Bubble.onDataDone);
+    sentimentHub.fetch();
+  },
 
-   // Triggered when data for a particular network is available
-   onData: function(network, data){
+  // Triggered when data for a particular network is available
+  onData: function(network, data){
 
-   },
+  },
 
-   // Triggered when we are done fetching data
-   onDataDone: function(data){
-       console.log('bubble on data done');
-       console.log(data);
-       Bubble.drawBubbles(data.slice(0,20));
-   },
+  // Triggered when we are done fetching data
+  onDataDone: function(data){
+    Bubble.drawBubbles(data.slice(0,20));
+  },
 
-   playIfShared: function(item){
-       var contentId = $.cookie("vgvidid");
-       if (contentId && item.id == contentId){
-           ga_events.sboxShareReferral(item.network, item.id);
-           if (item.type != 'video'){
-               player.hidePlayerMessage();
-               player.play(item);
-               ga_events.shareReferral('content:'+item.network, item.id);
-               $.cookie("vgvidid", '', {
-                   expires: 0,
-                   domain: cookieDomain,
-                   path: '/'
-               });
-           }
-       }
-   },
-
-   // draw the items as desired
-   drawItem: function(network, item){
-
-       // For this example I am creating a separate network div for each network data and
-       // append the items to the network div...
-       console.log('network', network);
-       var $networkDiv = $("."+network, Example.$container);
-       if ($networkDiv.length < 1) 
-           $networkDiv = $("<div></div>").addClass("network").addClass(network)
-               .append($("<h2></h2>").html(network))
-               .appendTo(Example.$container);
-
-       console.log("Item:", item);
-
-       // create a div and attach the raw item data so we can use it to open the player popup on click
-       var photo = item.image || item.thumb || item.profilePic;
-       $networkDiv.append(
-           $("<div></div>").addClass("item") 
-               .append(photo? $("<img />").attr({src: photo}) : '')
-               .append($("<div></div>").addClass("info").html(item.text))
-               .click(function(){
-                   player.play(item);
-               })
-               .data({item: item})
-       );
-   },
-
-   drawBubbles : function (data){
-       console.log('darwbubbles function');
-
-       var width = 960,
-           height = 500,
-          thumbr = 80, // this is the radius on width of thumbnails
-          centerRadius = Math.min(width/2-thumbr, height/2-thumbr);
-
-       var svg = d3.select("body").append("svg")
-           .attr("width", width)
-           .attr("height", height);
-
-        // var center = svg.append("circle")
-        // .attr('cx', width/2)
-        // .attr('cy', height/2)
-        // .attr('r', centerRadius)
-        // .attr('fill', 'black');
-
-        // inject a first element that all nodes will link to
-       data.unshift({});
-
-
-       var force = d3.layout.force()
-           .gravity(.3)
-           .distance(50)
-           .charge(-100)
-           .size([width, height]);
-
-
-
-      var centralLinks = [];
-      var centralLinksDistances = [];
-      for (var i = 1 ; i < data.length ; i++){
-        centralLinks.push({source: data[0], target: data[i], distance: centerRadius});
-        centralLinksDistances.push(centerRadius*2);
+  playIfShared: function(item){
+    var contentId = $.cookie("vgvidid");
+    if (contentId && item.id == contentId){
+      ga_events.sboxShareReferral(item.network, item.id);
+      if (item.type != 'video'){
+        player.hidePlayerMessage();
+        player.play(item);
+        ga_events.shareReferral('content:'+item.network, item.id);
+        $.cookie("vgvidid", '', {
+          expires: 0,
+          domain: cookieDomain,
+          path: '/'
+        });
       }
+    }
+  },
 
-      var contentLinks = [];
-      var contentLinksDistances = [];
-      for (var i = 1 ; i < data.length ; i++){
-        for (var j = 1 ; j < data.length ; j++){
-          contentLinks.push({source: data[i], target: data[j]});
-          contentLinksDistances.push(120);
-        }
+  // draw the items as desired
+  drawItem: function(network, item){
+    // For this example I am creating a separate network div for each network data and
+    // append the items to the network div...
+    console.log('network', network);
+    var $networkDiv = $("."+network, Example.$container);
+    if ($networkDiv.length < 1) {
+      $networkDiv = $("<div></div>").addClass("network").addClass(network)
+      .append($("<h2></h2>").html(network))
+      .appendTo(Example.$container);
+    }
+
+    console.log("Item:", item);
+
+    // create a div and attach the raw item data so we can use it to open the player popup on click
+    var photo = item.image || item.thumb || item.profilePic;
+    $networkDiv.append(
+      $("<div></div>").addClass("item") 
+      .append(photo? $("<img />").attr({src: photo}) : '')
+      .append($("<div></div>").addClass("info").html(item.text))
+      .click(function(){
+        player.play(item);
+      })
+      .data({item: item})
+    );
+  },
+
+  drawBubbles : function (data){
+    console.log('darwbubbles function');
+
+    //parameters
+    var width = 960;  // need to make reponsive
+    var height = 500;  // need to make reponsive
+    var thumbr = 40; // this is the radius on width of thumbnails
+    var centerRadius = Math.min(width/2-thumbr, height/2-thumbr);
+
+    var gravity = 0.3;
+    var distance = 50;
+    var charge = -1000;
+
+
+    //main container
+    var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+    // var center = svg.append("circle")
+    // .attr('cx', width/2)
+    // .attr('cy', height/2)
+    // .attr('r', centerRadius)
+    // .attr('fill', 'black');
+
+    // inject a first central element into data that all nodes will link to
+    data.unshift({x: width/2, y:height/2, fixed:true});
+
+    // force layout parametrization
+    var force = d3.layout.force()
+    .gravity(gravity)
+    .distance(distance)
+    .charge(charge)
+    .size([width, height]);
+
+
+    // to create links between center and everynode
+    var centralLinks = [];
+    for (var i = 1 ; i < data.length ; i++){
+      centralLinks.push({source: data[0], target: data[i], distance: centerRadius});
+    }
+
+    var contentLinks = [];
+    for (var i = 1 ; i < data.length ; i++){
+      for (var j = 1 ; j < data.length ; j++){
+        contentLinks.push({source: data[i], target: data[j]});
       }
+    }
 
-
-     // console.log(centralLinksDistances.concat(contentLinksDistances))
+    var allLinks = centralLinks.concat(contentLinks);
+    // for (var k = 0 ; k< allLinks.length ; k++){
+      // console.log('[source:',allLinks[0].source,'- target:', allLinks[0].target,']');
+      console.log('object',allLinks[0].target);
+      console.log('object.keys',Object.keys(allLinks[0].target));
+      console.log('object.hasOwnProperty(index)',allLinks[0].target.hasOwnProperty('index'));
+      console.log('object.index',allLinks[0].target.index);
+      // console.log(typeof arr[0].target['_used']);
+     // }
 
       // start should be called again whenever the nodes and links change again
       force.nodes(data)
-          .links(centralLinks.concat(contentLinks))
+          .links(centralLinks)
           .linkDistance(function(link, index){
               if (link.source.index === 0) {
-                return centerRadius *4;
+                return centerRadius * (1+Math.random()) ;
               } else {
-                return thumbr *4;
+                return thumbr * 4;
               }
           })
-          .linkStrength(0.3)
+          .linkStrength(function(link, index){
+              if (link.source.index === 0) {
+                return Math.max(Math.random(),0.5);
+              } else {
+                return 0.2;
+              }
+          })
           .start();
 
 
@@ -133,7 +147,7 @@ var Bubble = {
           .enter().append("line")
           .attr("class", "link");
 
-
+        // $(".link").css("stroke", "black");
 
          var node = svg.selectAll(".node")
              .data(data)
@@ -145,6 +159,7 @@ var Bubble = {
 
          color = "#33CCFF";
          node.append("circle")
+             .attr("class", "outercircle")
              .style("fill", function(d) {
                if (d.network === "Youtube") {
                  return "red";
@@ -163,20 +178,24 @@ var Bubble = {
              .attr("cy", 0);
 
          node.append("circle")
+             .attr("class", "innercircle")
              .style("fill", "black")
-             .attr("r", 40)
+             .attr("r", thumbr)
              .attr("cx", 0)
              .attr("cy", 0);
 
 
          node.append("clipPath")
              .attr("id", function(d) { return "innerCircle" + d.index; })
+             .attr("class", "clip")
              .append("circle")
-             .attr("r", 40)
+             .attr("class", "clipcircle")
+             .attr("r", thumbr)
              .attr("cx", 0)
              .attr("cy", 0);
 
          node.append("image")
+             .attr("class","image")
              .attr("xlink:href", function(d) { return d.thumb; })
              .attr("clip-path", function(d) { return "url(#innerCircle" + d.index + ")";})
              .attr("x", -50)
@@ -185,24 +204,94 @@ var Bubble = {
              .attr("height", 100);
 
 
-         // node.on("mouseover", function() {
-         //   d3.select(this).transition()
-         //     .style("fill", "white")
-         //     .attr("r", 40)
-         //     .attr("cx", 0)
-         //     .attr("cy", 0)
-         //   .duration(1000)
-         //   .delay(100);
+         node.on("mouseenter", function() {
+          console.log("mouseover");
+           d3.select(this).select(".outercircle").transition()
+             .attr("r", thumbr*2)
+           .duration(1000)
+           .delay(10);
+
+           d3.select(this).select(".innercircle").transition()
+           .attr("r", thumbr*2)
+           .duration(1000)
+           .delay(10);
+
+           d3.select(this).select(".clip").transition()
+             .attr("r", thumbr*2)
+           .duration(1000)
+           .delay(10);
+
+           d3.select(this).select(".clipcircle").transition()
+           .attr("r", thumbr*2)
+           .duration(1000)
+           .delay(10);
+
+           d3.select(this).select(".image").transition()
+           .attr("width", 200)
+          .attr("height", 200)
+          .attr("x",-100)
+          .attr("y",-100)
+           .duration(1000)
+           .delay(10);
+
 
            // d3.select(this).append("text")
            //   .attr("dx", -20)
            //   .attr("dy", 30)
            //   .text(function(d) { return d.contribName; } );
-         //});
+         });
 
-         // node.on("click", function(d){
-         //           player.play(d);
-         //       });
+          node.on("mouseleave", function() {
+          console.log("mouseleave");
+           d3.select(this).select(".outercircle").transition()
+             .attr("r", thumbr)
+           .duration(1000)
+           .delay(10);
+
+           d3.select(this).select(".innercircle").transition()
+           .attr("r", thumbr)
+           .duration(1000)
+           .delay(10);
+
+           d3.select(this).select(".clip").transition()
+             .attr("r", thumbr)
+           .duration(1000)
+           .delay(10);
+
+           d3.select(this).select(".clipcircle").transition()
+           .attr("r", thumbr)
+           .duration(1000)
+           .delay(10);
+
+           d3.select(this).select(".image").transition()
+           .attr("width", 100)
+          .attr("height", 100)
+          .attr("x",-50)
+          .attr("y",-50)
+           .duration(1000)
+           .delay(10);
+
+
+           // d3.select(this).append("text")
+           //   .attr("dx", -20)
+           //   .attr("dy", 30)
+           //   .text(function(d) { return d.contribName; } );
+         });
+
+
+
+           $("#playerBox_container").css("-webkit-transform", "scale(0.4,0.4)");
+           $("#playerBox_container").css("top", height/2);
+          $("#playerBox_container").css("left", width/2);
+           $("#playerBox_container").css("position", "absolute");
+          //  $("#playerBox_container").css("height", 200);
+ 
+
+
+
+         node.on("click", function(d){
+                   player.play(d);
+               });
 
 
          force.on("tick", function() {
