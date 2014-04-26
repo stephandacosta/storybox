17b054ce5bf75adb0239e6e0f4208f0f2aef4e1c
stephandacosta <stephan.dacosta.tech@gmail.com>
2014-04-25 21:30:51 -0700

    initial commit

--- bubbles.js
+++ bubbles.js
@@ -66,310 +66,144 @@ var Bubble = {
   drawBubbles : function (data){
     console.log('darwbubbles function');
 
-    // console.log(data[1]);
+    var colors = d3.scale.category10();
+    
+    SimpleBubble = function(d, id, c) {
+      this.data = d;
+      this.id = id;
+      this.canvas = c;
+      this.el = null;
+      this.x = 0;
+      this.y = 0;
+      this.radius = 0;
+      this.boxSize = 0;
+      this.isDragging = false;
+      this.isSelected = false;
+      this.tooltip = null;
 
-    //parameters
-    var width = 960;  // need to make reponsive
-    var height = 500;  // need to make reponsive
-    var thumbRadius = 40; // this is the radius on width of thumbnails
-    var centerRadius = Math.min(width/2-thumbRadius, height/2-thumbRadius);
-    var stroke = 5;
-
-    var gravity = 0.3;
-    var distance = 50;
-    var charge = -1000;
-
-    var centralLinksDistance = function(){
-      return centerRadius * (1+Math.random());
-    };
-    var distributedLinksDistance = function(){
-      return thumbRadiusadius * 2;
-    };
-    var centralLinksStrength = function(){
-      return Math.max(Math.random(),0.5);
-    };
-    var distributedLinksStrength = function(){
-      return 0.2;
+      this.init();
     };
 
-    //main container
-    var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    SimpleBubble.prototype.init = function() {
+      /* Elements that make up the bubbles display*/
+      this.el = $("<div class='bubble' id='bubble-" + this.id + "'></div>");
+      this.elFill = $("<div class='bubbleFill'></div>");
+      this.el.append(this.elFill);
 
-    // var center = svg.append("circle")
-    // .attr('cx', width/2)
-    // .attr('cy', height/2)
-    // .attr('r', centerRadius)
-    // .attr('fill', 'black');
+      /* Attach mouse interaction to root element */
+      /* Note use of $.proxy to maintain context */
+      this.el.on('mouseover', $.proxy(this.showToolTip, this));
+      this.el.on('mouseout', $.proxy(this.hideToolTip, this));
 
-    // inject a first central element into data that all nodes will link to
-    data.unshift({x: width/2, y:height/2, fixed:true});
+      /* Set CSS of Elements  */
+      this.radius = this.data;
+      this.boxSize = this.data * 2;
 
-    // force layout parametrization
-    var force = d3.layout.force()
-    .gravity(gravity)
-    .distance(distance)
-    .charge(charge)
-    .size([width, height]);
+      this.elFill.css({
+        width: this.boxSize,
+        height: this.boxSize,
+        left: -this.boxSize / 2,
+        top: -this.boxSize / 2,
+        "background-color": colors(this.data)});
+    };
 
+    SimpleBubble.prototype.showToolTip = function() {
+      // var toolWidth = 40;
+      // var toolHeight = 25;
+      // this.tooltip =  $("<div class='tooltip'></div>");
+      // this.tooltip.html("<div class='tooltipFill'><p>" + this.data + "</p></div>");
+      // this.tooltip.css({
+      //   left: this.x + this.radius /2,
+      //   top: this.y + this.radius / 2
+      //   });
+      // this.canvas.append(this.tooltip);
+    };
 
-  // Links arrays
-    // links from center
-    var centralLinks = [];
-    for (var i = 1 ; i < data.length ; i++){
-      centralLinks.push({source: data[0], target: data[i], distance: centerRadius});
-    }
-    // distributed links
-    var contentLinks = [];
-    for (var i = 1 ; i < data.length ; i++){
-      for (var j = 1 ; j < data.length ; j++){
-        contentLinks.push({source: data[i], target: data[j]});
-      }
-    }
-    //concatentation of center and distributed links
-    var allLinks = centralLinks.concat(contentLinks);
+    SimpleBubble.prototype.hideToolTip = function() {
+      $(".tooltip").remove();
+    };
 
-  /* *** test on getting links indexes (delete)
-      console.log('object',allLinks[0].target);
-      console.log('object.keys',Object.keys(allLinks[0].target));
-      console.log('object.hasOwnProperty(index)',allLinks[0].target.hasOwnProperty('index'));
-      console.log('object.index',allLinks[0].target.index);
-  // end delete */
+    SimpleBubble.prototype.move = function() {
+      this.el.css({top: this.y, left:this.x});
+    };
 
-      // start should be called again whenever the nodes and links change again
-    force.nodes(data)
-    .links(centralLinks)
-    .linkDistance(function(link, index){
-      if (link.source.index === 0) {
-        return centralLinksDistance() ;
-      } else {
-        return distributedLinksDistance();
-      }
-    })
-    .linkStrength(function(link, index){
-        if (link.source.index === 0) {
-          return centralLinksStrength();
-        } else {
-          return distributedLinksStrength();
-        }
-    })
-    .start();
+    SimpleVis = function(container,d) {
+      this.width = 800;
+      this.height = 400;
+      this.canvas = container;
+      this.data = d;
+      this.force = null;
+      this.bubbles = [];
+      this.centers = [
+      {x: 200, y:200},
+      {x: 400, y:200},
+      {x: 600, y:200}
+      ];
 
-    /* uncomment to create links in DOM
-    var link = svg.selectAll(".link")
-    .data(centralLinks)
-    .enter().append("line")
-    .attr("class", "link");
-    $(".link").css("stroke", "black");
-    */
+      this.bin = d3.scale.ordinal().range([0,1,2]);
 
-    // create nodes in DOM
-    var node = svg.selectAll(".node")
-    .data(data)
-    .enter().append("g")
-    .attr("class", "node")
-    .call(force.drag);
+      this.bubbleCharge = function(d) {
+        return -Math.pow(d.radius,1) * 8;
+      };
 
-    var networkColors = {
-      Youtube: "red",
-      Twitter: "cyan",
-      Instagram: "blue",
-      VGVideo: "magenta",
-      Photo: "green"
+      this.init();
+    };
+
+    SimpleVis.prototype.init = function() {
+      /* Store reference to original this */
+      var me = this;
+
+      /* Initialize root visualization element */
+      this.canvas.css({
+        width: this.width,
+        height: this.height,
+        "background-color": "#eee",
+        position: "relative"});
+
+      /* Create Bubbles */
+      for(var i=0; i< this.data.length; i++) {
+        var b = new SimpleBubble(this.data[i], i, this.canvas);
+        /* Define Starting locations */
+        b.x = b.boxSize + (20 * (i+1));
+        b.y = b.boxSize + (10 * (i+1));
+        this.bubbles.push(b);
+        /* Add root bubble element to visualization */
+        this.canvas.append(b.el);
+      };
+
+      /* Setup force layout */
+      this.force = d3.layout.force()
+        .nodes(this.bubbles)
+        .gravity(0)
+        .charge(this.bubbleCharge)
+        .friction(0.87)
+        .size([this.width, this.height])
+        .on("tick", function(e) {
+          me.bubbles.forEach( function(b) {
+            me.setBubbleLocation(b, e.alpha, me.centers);
+            b.move();
+          });
+        });
+
+      this.force.start();
+    };
+
+    SimpleVis.prototype.setBubbleLocation = function(bubble, alpha, centers) {
+      var center = centers[this.bin(bubble.id)];
+      bubble.y = bubble.y + (center.y - bubble.y) * (0.115) * alpha;
+      bubble.x = bubble.x + (center.x - bubble.x) * (0.115) * alpha;
     };
 
 
-    node.append("circle")
-    .attr("class", "innercircle")
-    .style("stroke", function(d) { return d.index===0? "black" : networkColors[d.network];})
-    .style("stroke-width", stroke)
-    .style("fill", function(d) {return d.index===0? "white" : "black";})
-    .attr("r", thumbRadius)
-    .attr("cx", 0)
-    .attr("cy", 0);
+      var vis = new SimpleVis($("body"), [12,33,20,40,60,10,25,44,13,23,14,25,8]);
 
-
-    node.append("clipPath")
-    .attr("id", function(d) { return "innerCircle" + d.index; })
-    .attr("class", "clip")
-    .append("circle")
-    .attr("class", "clipcircle")
-    .attr("r", thumbRadius-stroke/2)
-    .attr("cx", 0)
-    .attr("cy", 0);
-
-    node.append("image")
-    .attr("class","image")
-    .attr("xlink:href", function(d) { return d.thumb; })
-    .attr("clip-path", function(d) { return "url(#innerCircle" + d.index + ")";})
-    .attr("x", -thumbRadius)
-    .attr("y", -thumbRadius)
-    .attr("width", thumbRadius*2)
-    .attr("height", thumbRadius*2);
-
- 
-    node.append('text')
-    .attr()
-    .attr("fill","black")
-    .text(function(d){return d.index===0 ? "test" : "";});
-
-    node.on("mouseenter", function() {
-
-      // work on central circle
-      d3.select("circle").transition()
-      .attr("r", centerRadius)
-      .style("z-index", 10)
-      .duration(500)
-      .delay(0);
-      
-      // get text from moused enter
-      d3.select(this).attr("text", function(d){return d.textHtml;});
-      var msg = d3.select(this).attr("text");
-
-      d3.select("text").transition()
-      .text(msg)
-      .attr("font-size", "12")
-      .duration(500)
-      .delay(0);
-
-
-      d3.select(this).select(".innercircle").transition()
-      .attr("r", thumbRadius*1.5)
-      .style("z-index", 10)
-      .duration(500)
-      .delay(0);
-
-      d3.select(this).select(".clip").transition()
-      .attr("r", thumbRadius*1.5)
-      .style("z-index", 10)
-      .duration(500)
-      .delay(0);
-
-      d3.select(this).select(".clipcircle").transition()
-      .attr("r", thumbRadius*1.5)
-      .style("z-index", 10)
-      .duration(500)
-      .delay(0);
-
-      d3.select(this).select(".image").transition()
-      .style("z-index", 10)
-      .attr("width", thumbRadius*2*1.5)
-      .attr("height", thumbRadius*2*1.5)
-      .attr("x",-thumbRadius*1.5)
-      .attr("y",-thumbRadius*1.5)
-      .duration(500)
-      .delay(0);
-
-      // d3.select(this).append("text")
-      //   .attr("dx", -20)
-      //   .attr("dy", 30)
-      //   .text(function(d) { return d.contribName; } );
-    });
-
-    node.on("mouseleave", function() {
-
-      d3.select("circle").transition()
-      .attr("r", thumbRadius)
-      .style("z-index", 5)
-      .duration(500)
-      .delay(0);
-
-      d3.select("text").transition()
-      .attr("font-size", "1")
-      .duration(500)
-      .delay(0);
-
-      d3.select(this).select(".innercircle").transition()
-      .style("z-index", 5)
-      .attr("r", thumbRadius)
-      .duration(1000)
-      .delay(10);
-
-      d3.select(this).select(".clip").transition()
-      .style("z-index", 5)
-       .attr("r", thumbRadius)
-      .duration(1000)
-      .delay(10);
-
-      d3.select(this).select(".clipcircle").transition()
-      .style("z-index", 5)
-      .attr("r", thumbRadius)
-      .duration(1000)
-      .delay(10);
-
-      d3.select(this).select(".image").transition()
-      .style("z-index", 5)
-      .attr("width", 100)
-      .attr("height", 100)
-      .attr("x",-50)
-      .attr("y",-50)
-      .duration(1000)
-      .delay(10);
-
-     // d3.select(this).append("text")
-     //   .attr("dx", -20)
-     //   .attr("dy", 30)
-     //   .text(function(d) { return d.contribName; } );
-    });
-
-
-    // player configuration
-    // $("#playerBox_container").css("-webkit-transform", "scale(0.4,0.4)");
-    // $("#playerBox_container").css("top", height/2);
-    // $("#playerBox_container").css("left", width/2);
-    // $("#playerBox_container").css("position", "absolute");
-    //  $("#playerBox_container").css("height", 200);
-
-
-
-    // click event --> plays video
-    node.on("click", function(d){
-      player.play(d);
-    });
-
-    // time interval to enable continuous movement
-    setInterval(function(){
-      console.log("moving");
-      // force.alpha(0.1);
-      node.attr("x", function(d){return d.x+Math.random()*5;})
-      .attr("y", function(d){
-        if (d.y >= height/2){
-          return d.y-10;
-        } else {
-          return d.y+10;
-        }
+      $("#move").on("click", function(e) {
+        vis.bin.range(vis.bin.range().reverse());
+        vis.force.resume();
+        return false;
       });
-    }, 200);
 
 
-    force.on("tick", function() {
-
-      /* uncomment to show links - links repositionning
-      link.attr("x1", function(d) { return d.source.x; })
-      .attr("y1", function(d) { return d.source.y; })
-      .attr("x2", function(d) { return d.target.x; })
-      .attr("y2", function(d) { return d.target.y; });
-      */
-
-      // circles repositionning
-      node.attr("cx", function(d) { 
-        if (d.index === 0 ){
-          return d.x = (width/2);
-        } else {
-          return d.x = Math.max(thumbRadius+5, Math.min(width-thumbRadius-5, d.x));
-        }
-      });
-      node.attr("cy", function(d) { 
-        if (d.index === 0 ){
-          return d.y = (height/2);
-        } else {
-          return d.y = Math.max(thumbRadius+5, Math.min(height-thumbRadius-5, d.y));
-        }
-      });
-      node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-
-    });
 
   }
 
